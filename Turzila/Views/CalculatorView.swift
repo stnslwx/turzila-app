ac0ce96f865ee96384d2a8059b9030e56f95643a
Stanislaw <souldocker@gmail.com>
2023-08-14 23:15:11 +0300

    CalculatorView

---
+++ Turzila/Views/CalculatorView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct CalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorView()
+    }
+}
